aaa86f4df1291fccfe00f360aab07cbc3a989d60
Mila18-star <melaniaivankova@yandex.ru>
2024-11-13 13:20:53 +0300

    задала структуру файла main

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 ## main стартовый модуль проекта
 #project 4
+# TODO import <зависимость из задания>
+
+def main():
+    #TODO - сделай вызов функций из functions
+    pass
+
+# инициализационный скрипт
+if __name__=="__main__":
+    main()
