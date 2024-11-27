c44df93a65e770476ff37911737da580d86d1e04
AlenaHud <Alenaa.hud@yandex.ru>
2024-11-27 11:47:56 +0300

    Сделали мердж

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 ##стартовый модуль проекта
 
+ 
+from functions import fanc1, fanc2   #TODO переименовать функции
 
 from functions import fanc1, fanc2   #TODO переименовать функции
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 def main():
   pass
 
 if __name__ == "__main__":
   pass
 
+<<<<<<< Updated upstream
  
+=======
+>>>>>>> Stashed changes
