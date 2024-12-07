93b8cdb3df52e375625d60dcdb02a41a46581737
AlenaHud <Alenaa.hud@yandex.ru>
2024-12-07 18:34:52 +0300

    вернула исправный стартовый модуль

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 ##стартовый модуль проекта
 
- 
 from functions import fanc1, fanc2   #TODO переименовать функции
 
 from functions import fanc1, fanc2   #TODO переименовать функции
 
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 def main():
   pass
 
 if __name__ == "__main__":
   pass
-
-<<<<<<< Updated upstream
- 
-=======
->>>>>>> Stashed changes
