6fb626fe4fd2209cba25938b6edadb3e50c04413
AlenaHud <Alenaa.hud@yandex.ru>
2024-12-10 10:31:45 +0300

    написала код для вызова функции

--- main.py
+++ main.py
@@ -2,13 +2,9 @@
 #project 4
 
 # TODO - переименовать функции
-from functions import func1, func2
-
-def main():
-    #TODO - сделай вызов функций из functions
-    pass
-
+from functions import apply_changes
+ 
 # инициализационный скрипт
 if __name__=="__main__":
-    main()
+    apply_changes()
  
